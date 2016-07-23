1eb4a3056f5200b734c878d110c79978a575072b
oldchevy <nick.olszowy@gmail.com>
2016-07-22 19:04:17 -0700

    Completes basic parseable fixtures for parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -7,10 +7,25 @@ var parseJSON = function(json) {
 	//or array, to call a recursive function which returns that entire new object parsed
 	var building;
 
+	json = stripWhiteSpace(json);
 	var objects = json.match(/^{(.*)}$/); //Greedy method makes sure only the outermost is selected
   	var arrays = json.match(/^\[(.*)\]$/);
   
   	//console.log(objects, arrays);
+
+  	function stripWhiteSpace(entry){
+  		var quotes = entry.match(/\".*?\"/g);
+  		var strip = entry.replace(/[\s\n\t\r]/g,"");
+      	var tooFar = strip.match(/\".*?\"/g);
+      	if(quotes){
+      		tooFar.forEach(function(entry,index){
+          		strip = strip.replace(entry, quotes[index]);
+        	});
+      	}
+
+  		return strip;
+  	}
+
 
   	function findVal(entry){
   		var oneMatch;
@@ -22,8 +37,14 @@ var parseJSON = function(json) {
   			return true;
   		if(entry.match(/^false$/))
   			return false;
-  		if(oneMatch = entry.match(/^"(.*)"$/))
-  			return oneMatch[1];
+  		if(entry.match(/^null$/))
+  			return null;
+  		if(oneMatch = entry.match(/^\"(.*)\"$/)){
+  			//if(!oneMatch[1])
+  				//return oneMatch[0];
+  			//else
+  				return oneMatch[1];
+  		}
   		if(entry.match(/^[+-]?(\d+|\d*\.\d+|\d+\.\d*)([eE][+-]?\d+)?$/))
   			//I made this in eloquent javascript chapter on regEx
   			return parseFloat(entry);
@@ -36,15 +57,15 @@ var parseJSON = function(json) {
 
       	while(bool){
       		bool = false;
-      		var nextEntry = workingObj.match(/^("(.*?)":([0-9\.eE+-]+|\".*?\"|\[.*?\])),(.*)/) ||
+      		var nextEntry = workingObj.match(/^("(.*?)":([0-9\.eE+-]+|\".*?\"|\[.*\]|\{.*\}|null|false|true)),(.*)/) ||
       						workingObj.match(/^("(.*?)":(.*))/);
       		console.log(nextEntry);
         	if(nextEntry){
         		building[nextEntry[2]] = findVal(nextEntry[3]);
-        	}
-        	if(nextEntry[4]){
-        		workingObj = nextEntry[4];
-        		bool = true;
+        		if(nextEntry.length > 4){
+        			workingObj = nextEntry[4];
+        			bool = true;
+        		}
         	}
       	}
     }
@@ -56,10 +77,11 @@ var parseJSON = function(json) {
 
       	while(bool){
       		bool = false;
-      		var nextEntry = workingObj.match(/^(\".*?\"),(.*)/) ||
+      		var nextEntry = workingObj.match(/^(null|false|true),(.*)/) ||
+      						workingObj.match(/^(\".*?\"),(.*)/) ||
       						workingObj.match(/^(\[.*\]),(.*)/) ||
       						workingObj.match(/^(\{.*\}),(.*)/) ||
-      						workingObj.match(/^([+-]?(\d+|\d*\.\d+|\d+\.\d*)([eE][+-]?\d+)?),(.*)/) ||
+      						workingObj.match(/^([+-]?(\d+|\d*\.\d+|\d+\.\d*)([eE][+-]?\d+)?),[ \t\r\n]*(.*)/) ||
       						workingObj.match(/(.*)/);
       						//Use short circuit evaluation to make sure
       						//we don't accidentally grab an entry inside a larger
